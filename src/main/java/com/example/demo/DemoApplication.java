10fe97f6cd56b3e27f0995d752c2f2dfbea7dec9
Omar Alnader <omaralnader@gmail.com>
2022-07-28 10:55:46 -0400

    1st change

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	//1st GitHub submission
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
   }
